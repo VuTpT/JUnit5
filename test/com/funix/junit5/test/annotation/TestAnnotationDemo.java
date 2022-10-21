5d03eb12c96959fe63ce2ea564327d5c6c343438
VuTpT <53907551+VuTpT@users.noreply.github.com>
2022-10-21 23:34:26 +0700

    Create TestAnnotationDemo.java

---
+++ test/com/funix/junit5/test/annotation/TestAnnotationDemo.java
@@ -0,0 +1,14 @@
+package com.funix.junit5.test.annotation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestAnnotationDemo {
+	
+	@Test
+	public void test() {
+		// Checks that two primitives/objec 
+		assertEquals(3, 2);
+	}
+}
